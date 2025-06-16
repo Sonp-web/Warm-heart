95937f9dcb69e1be32db7ffd7e7166251f607032
Egor <pattrixd@yandex.com>
2025-06-16 16:31:47 +0300

    update project

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
 import "../sass/style.scss";
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const menu = document.querySelectorAll(".footer__menu__item"),
+    submenu = document.querySelectorAll(".footer__menu__item__submenu");
+
+    
+})
